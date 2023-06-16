c98168343cc5842458f9feb681a124ce6ee25b32
shalinispk7 <shalinispk7@gmail.com>
2023-06-16 09:29:27 +0530

    modified css changes

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var owl = $('.gal-yummy');
 owl.owlCarousel({
-    items:5,
+  
     loop:true,
     margin:10,
     autoplay:true,
     autoplayTimeout:1000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+	responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
@@ -15,13 +30,30 @@ $('.stop').on('click',function(){
 })
 
 var owl = $('.event-yummy');
+
 owl.owlCarousel({
-    items:3,
+    
     loop:true,
     margin:10,
     autoplay:false,
     autoplayTimeout:6000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[6000])
@@ -33,12 +65,27 @@ $('.stop').on('click',function(){
 
 var owl = $('.brand');
 owl.owlCarousel({
-    items:1,
+    
     loop:true,
     margin:10,
     autoplay:true,
     autoplayTimeout:1000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+	responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
